b80eeea7bedc64e7f845ed6b19e3956343f0e35f
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-03 21:51:50 +0530

    The homepage

---
+++ Homepage.svelte
@@ -0,0 +1,117 @@
+<div class="image-container">
+	<div class="image-left">
+		<img src="bg2.jpg" alt="Left Image" width="450" height="350" />
+		<p>Residential Constructions</p>
+	</div>
+	<div class="image-right">
+		<img src="bg3.jpg" alt="Right Image" width="450" height="350" />
+		<p>Commercial Constructions</p>
+	</div>
+
+	<div class="image-right">
+		<img src="bg4.jpeg" alt="Right Image" width="450" height="350" />
+		<p>Industrial Constructions</p>
+	</div>
+</div>
+
+<div class="text">
+	<h2>About 4F Builders & Engineering Private Limited</h2>
+	<p>
+		4F Builders & Engineering Private Limited, incorporated on October 9, 2020, is a distinguished
+		player in the construction and engineering services sector. As a privately-held company
+		operating independently, it adheres to regulatory standards and maintains accountability within
+		the legal framework. Registered with the Registrar of Companies in Ernakulam, the company
+		demonstrates a commitment to compliance.
+		<br /><br /> With an authorized share capital of Rs. 100,000 and a paid-up capital of Rs.
+		100,000, 4F Builders & Engineering Private Limited specializes in complete construction projects
+		and civil engineering activities. Its most recent Annual General Meeting (AGM) date is not
+		available, and its latest balance sheet was filed on March 31, 2022, according to Ministry of
+		Corporate Affairs (MCA) records.
+		<br /><br />
+		The company's board of directors, consisting of Aji Somarajan and Prasanth Bhanu, provides strategic
+		direction and leadership to drive its objectives and growth agenda.
+	</p>
+</div>
+<div>
+	<h3>Our Details</h3>
+	<ul className="lsty">
+		<li>Corporate Identification Number:(CIN) U45202KL2020PTC064932</li>
+		<li>Registration Number: 64932</li>
+		<li>Email Address: 4fbuildersengineering@gmail.com</li>
+		<li>
+			Address: X/231, Ambadiyil, Poozhikhadu, Kudassanadu P.O. Pandalam ,Pathanamthitta, 689512
+		</li>
+	</ul>
+</div>
+
+<style>
+	.text {
+		background-color: white;
+		margin-bottom: 0px;
+		font-size: 20px;
+		margin: 0 auto -20px;
+		font-family: poppins;
+	}
+
+	h3 {
+		font-size: 25px;
+		padding-top: 10px;
+		color: rgb(255, 255, 255);
+	}
+
+	.image-left,
+	.image-right {
+		text-align: center;
+		font-size: 20px;
+		color: white;
+		font-weight: bold;
+
+		font-family: Comfortaa;
+	}
+	.image-container {
+		margin: 0px;
+		padding: 0px;
+		margin-bottom: 0px;
+
+		padding-top: 50px;
+		background-color: black;
+		display: flex;
+		justify-content: space-around;
+	}
+
+	h1 {
+		font-size: 50px;
+		font-weight: bold;
+		color: rgb(0, 0, 0);
+		padding: 10px;
+	}
+
+	h2 {
+		color: rgb(0, 0, 0);
+
+		font-size: 30px;
+		padding: 10px;
+		margin-top: 0;
+		font-family: POPPINS;
+	}
+	li {
+		padding-left: 10;
+		font-family: POPPINS;
+		color: white;
+		font-size: 15;
+		margin-bottom: 15px;
+	}
+	p {
+		padding-left: 10px;
+	}
+
+	.bottom {
+		text-align: center;
+		color: wheat;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 15px;
+	}
+	.image-logo {
+		padding: 10px;
+	}
+</style>
