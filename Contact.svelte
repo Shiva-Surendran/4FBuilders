4f98a36a20ed50f740c9b37baaa42157acfaccd6
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-05 20:15:21 +0530

    Create Contact.svelte

---
+++ Contact.svelte
@@ -0,0 +1,44 @@
+<div class="bar">
+	<h1 class="h1">CONTACT US</h1>
+	<span class="texthead">Head Office: </span>
+	<span class="text"
+		>Ambadiyil bldg. Poozhikkadu, Pandalam, Pathanamthitta dist. Kerala. India. Pin Code: 689 501</span
+	><br /><br />
+	<span class="texthead">Tel: </span><span class="text">+91 4734254802</span><br /><br />
+	<span class="texthead">Email: </span><span class="text">4fbuildersengineering@gmail.com</span>
+</div>
+
+<style>
+	h1,
+	h2,
+	h3 {
+		color: rgb(255, 247, 247);
+		font-family: DM SANS;
+	}
+
+	.h1 {
+		font-size: 60px;
+		font-stretch: expanded;
+		color: rgb(0, 0, 0);
+		margin-bottom: 85px;
+	}
+	.text {
+		color: white;
+		font-size: 25px;
+		font-family: DM SANS;
+	}
+
+	.texthead {
+		font-weight: bold;
+		color: black;
+		font-size: 25px;
+		font-family: DM SANS;
+	}
+	.bar {
+		overflow: hidden;
+		text-align: center;
+		background-color: #6420AA;
+		box-shadow: 5px 2px 4px rgba(0, 0, 0, 0.1);
+		padding: 70px;
+	}
+</style>
