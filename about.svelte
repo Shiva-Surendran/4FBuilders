bfe040560e0bd60e9a153e131cab104a9b0f6b3d
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-09 19:43:50 +0530

    Create about.svelte

---
+++ about.svelte
@@ -0,0 +1,230 @@
+<div class="image-container">
+	<div class="text-right">
+		<h1 class="service">Our Services</h1>
+		<div class="service-box">
+			<div class="box">
+                <h3>DRAWINGS AND 3D MODELING</h3>
+            </div>
+            <div class="box">
+                <h3>ONLINE PERMITS</h3>
+            </div>
+            <div class="box">
+                <h3>INTERIOR DESIGNING</h3>
+            </div>
+            <div class="box">
+                <h3>ESTIMATION AND SCHEDULING</h3>
+            </div>
+            <div class="box">
+                <h3>SUPERVISION AND EXECUTION</h3>
+            </div>
+            <div class="box">
+                <h3>MEP WORK</h3>
+            </div>
+            <div class="box">
+                <h3>INTERLOCK PAVING AND LANDSCAPING</h3>
+            </div>
+            <div class="box">
+                <h3>STEEL ROOF (TRUSS) FABRICATION AND ERECTION</h3>
+            </div>
+            <div class="box">
+                <h3>ROOF SHEETING AND TILES</h3>
+            </div>
+            <div class="box">
+                <h3>WATERPROOFING</h3>
+            </div>
+            <div class="box">
+                <h3>ANTI-TERMITE TREATMENT</h3>
+            </div>
+            <div class="box">
+                <h3>KITCHEN CABINETS</h3>
+            </div>
+            <div class="box">
+                <h3>GYPSUM WORK</h3>
+            </div>
+            <div class="box">
+                <h3>RENOVATION WORK</h3>
+            </div>
+            
+		</div>
+	</div>
+</div>
+
+    <div class="image-container3">
+        <br />
+        <h1 class="MEET">MEET OUR TEAM</h1>
+        <div class="image-container2">
+            <div class="img">
+                <a href="./PRASANTH">
+                    <img src="cc2.png" alt="Right Image" width="350" height="350" />
+                </a>
+                <p>Prasanth Bhanu</p>
+            </div>
+            <div class="img">
+                <a href="./AJI">
+                    <img src="cc3.png" alt="Right Image" width="350" height="350" />
+                </a>
+                <p>Aji Somarajan</p>
+            </div>
+            <div class="img">
+                <a href="./Adarsh">
+                    <img src="cc.png" alt="Right Image" width="350" height="350" />
+                </a>
+                <p>Adarsh A K</p>
+            </div>
+            <div class="img">
+                <a href="./PRAVEEN">
+                    <img src="cc4.png" alt="Right Image" width="350" height="350" />
+                </a>
+                <p>Praveen P K</p>
+            </div>
+        </div>
+    </div>
+
+<style>
+	.service-box {
+		align-items: center;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	p {
+		font-size: 30px;
+	}
+
+	.box {
+		width: calc(33.33% - 20px);
+		margin: 10px;
+		padding: 20px;
+		background-color: #00ADB5;
+		border-radius: 10px;
+		text-align: center;
+		font-family: comfortaa;
+		box-shadow: 200 10 100px rgba(0, 0, 0, 0.1);
+	}
+
+	.box h3 {
+		margin: 0;
+		font-size: 18px;
+		color: #EEEEEE;
+	}
+
+	.boldw {
+		color: yellow;
+		font-size: 26px;
+	}
+
+	.spant {
+		font-size: 20px;
+		font-family: lora;
+	}
+
+	.image-left {
+		margin-top: 10px;
+	}
+
+	.service {
+		font-family: merriweather;
+		font-weight: bold;
+		font-size: 50px;
+		position: relative;
+		right: 55%;
+		color: EEEEEE;
+	}
+
+	.vision {
+		text-align: center;
+		font-family: rubik;
+		font-size: 36px;
+	}
+
+	.heading {
+		text-align: top;
+	}
+
+	.img {
+		text-align: center;
+		font-size: 20px;
+		color: white;
+		font-weight: bold;
+		font-family: LORA;
+		float: left;
+		margin-left: 50px;
+		margin-right: 50px;
+		margin-bottom: 20px;
+	}
+
+	.mission {
+		font-size: 20px;
+		color: white;
+		padding: 20px;
+		margin-bottom: 20px;
+		border-radius: 10px;
+		background-image: url(ss3.jpg);
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+
+	.circle {
+		border-radius: 50%;
+		width: 350px;
+		height: 250px;
+		background-color: rgba(0, 0, 0, 0.8); 
+		padding: 20px;
+		opacity: 1;
+		text-align: center;
+		margin: 20px;
+		transition: transform 0.5s ease-in-out;
+		color: white;
+
+		transition: transform 0.5s ease-in-out; 
+	}
+
+	.circle:hover {
+		transform: scale(1.1);
+	}
+
+	.image-container {
+		align-items: center;
+		display: flex;
+		color: white;
+		background-image: url(pattern.jpg);
+		margin-bottom: 20px;
+		padding: 20px;
+		border-radius: 10px;
+	}
+
+	.image-container2 {
+		align-items: center;
+		display: flex;
+		color: white;
+
+		margin-left: 32px;
+        
+	}
+
+	.text-right {
+		float: left;
+		text-align: right;
+		margin-left: 5px;
+	}
+
+	.ul {
+		justify-content: center;
+		text-align: left;
+		font-size: 28px;
+		font-family: Rubik;
+	}
+
+	.MEET {
+		color: white;
+		font-family: RUBIK;
+		font-size: 50px;
+		margin-top: 0;
+	}
+
+	.image-container3 {
+		text-align: center;
+		margin-top: 10px;
+		background-image: url(ss2.jpg);
+	}
+</style>
