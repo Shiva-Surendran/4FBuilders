5cd067fbd22f00edde9badea23cce66c9df696ac
Shiva-Surendran <107458185+Shiva-Surendran@users.noreply.github.com>
2024-03-20 08:31:35 +0530

    Create P1.svelte for prasanth bhanu page

---
+++ P1.svelte
@@ -0,0 +1,134 @@
+<div class="container">
+	<div class="top-navbar"></div>
+    <div class="image-container">
+        <img src="cc2.png" alt="Right Image" width="350" height="350" />
+    </div>
+	<div class="main-content">
+		<div class="box">
+			<h1 class="head2">Prasanth Bhanu</h1></div>
+			<div class="box2">
+
+		<h3 class="head">Managing Director</h3></div>
+		<h2>About Me</h2>
+		<p class="text">
+			A civil engineering professional with over 19 years of experience in various projects
+			including commercial multi-story buildings, residential villas, and industrial projects at
+			both national and international levels.
+		</p>
+		<h2>Experience in</h2>
+		<ul>
+			<li>India</li>
+			<li>UAE</li>
+			<li>Oman</li>
+		</ul>
+
+		<h2>I have worked in</h2>
+		<ul class="text">
+			<li>Kanakiya Construction Pvt. Ltd, Mumbai</li>
+			<li>AE Popular Interiors Emirates Heavy Engineering</li>
+			<li>KSA Shapoorji Pallonji And Company Pvt. Ltd.</li>
+			<li>Douglas Ohi LLC</li>
+			<li>Manarat Al Rayan LLC</li>
+			<li>Petron Gulf LLC</li>
+		</ul>
+	</div>
+
+	<div class="bottom-navbar"></div>
+</div>
+
+<style>
+	.head {
+		color: rgb(255, 255, 255);
+		font-size: 31px;
+	}
+	h3 {
+		text-align: center;
+	}
+h2{color:#F4CE14}
+	.head2 {
+		color: rgb(0, 0, 0);
+	}
+	h2,
+	h3,
+	h1 {
+		font-family: DM SANS;
+	}
+	.image-container {
+		display: flex;
+		justify-content: center;
+		margin-bottom: 20px;
+	}
+	.text {
+		font-family: dm sans;
+		font-size: 20px;
+		font-weight: bold;
+		color: black;
+	}
+	.container {
+		background-color: #FF2E63;
+		color: white;
+		font-family: Arial, sans-serif;
+		padding: 20px;
+		border-radius: 10px;
+		max-width: 600px;
+		margin: 0 auto;
+	}
+
+	.top-navbar {
+		background-color: #efefef;
+		padding: 10px;
+		border-radius: 5px;
+		margin-bottom: 20px;
+	}
+
+	.bottom-navbar {
+		background-color: #efefef;
+		padding: 10px;
+		border-radius: 5px;
+		margin-top: 20px;
+	}
+
+	h1 {
+		font-size: 36px;
+		text-align: center;
+		margin-bottom: 10px;
+	}
+
+	h2
+	 {
+		font-size: 24px;
+		margin-bottom: 0px;
+	}
+
+	p {
+		font-size: 16px;
+		line-height: 1.5;
+		margin-bottom: 10px;
+	}
+
+	ol,
+	ul {
+		padding-left: 20px;
+		margin-bottom: 10px;
+font-family: dm sans;		color: black;
+	}
+
+	li {
+		font-size: 20px;
+		font-family: dm sans;
+		font-weight: bolder;
+	}
+	.box {
+    background-color: #ffffff; 
+    padding: 1px;
+    border-radius: 5px;
+    margin-bottom: 0px; 
+    text-align: center; 
+}
+.box2 {
+    background-color: #811111; 
+    padding: 1px;
+    border-radius: 5px;
+    text-align: center; 
+}
+</style>
